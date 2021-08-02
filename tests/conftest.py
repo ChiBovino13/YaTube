d627627c0835d1f81e982a41e8867d22f054e18a
yandex-praktikum <58176914+yandex-praktikum@users.noreply.github.com>
2021-08-02 20:51:40 +0300

    removed migrations check

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,6 +1,5 @@
 import sys
 import os
-from subprocess import Popen, PIPE
 
 
 root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -28,10 +27,6 @@ if FILENAME not in project_dir_content:
         f'В директории `{MANAGE_PATH}` не найден файл `{FILENAME}`. '
         f'Убедитесь, что у вас верная структура проекта.'
     )
-
-process = Popen(['python', 'yatube/manage.py', 'makemigrations', '--check', '--dry-run', '--no-input'], stdout=PIPE, stderr=PIPE)
-stdout, _ = process.communicate()
-assert process.returncode == 0, f'Вы забыли сделать миграции.\n\n{stdout.decode("UTF-8")}'
 
 pytest_plugins = [
     'tests.fixtures.fixture_user',
